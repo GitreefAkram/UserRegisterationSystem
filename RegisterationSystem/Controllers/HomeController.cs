ee3a091de7763a5c6a135c27753e0bfef6882203
Gitreef akram <gitreefa12@gmail.com>
2021-09-10 14:20:55 +0500

    User can be viewed in a list from where they can be edited & deleted
    Copied from main to repository

---
+++ RegisterationSystem/Controllers/HomeController.cs
@@ -0,0 +1,140 @@
+﻿using RegisterationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegisterationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(tbl_users user)
+        {
+            Entities db = new Entities();
+            var data = db.tbl_users.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+            if (data != null)
+            {
+                return RedirectToAction("UsersList");
+            }
+            else
+            { 
+            return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(tbl_users user)
+        {
+            Entities db = new Entities();
+            db.tbl_users.Add(user);
+            db.SaveChanges();
+
+
+            return RedirectToAction("UsersList");
+        }
+
+        public ActionResult UsersList()
+        {
+            Entities db = new Entities();
+            var users = db.tbl_users.ToList();
+    
+            return View(users);
+        }
+        
+        public ActionResult UserProfile(int id)
+        {
+            Entities db = new Entities();
+            var user = db.tbl_users.Find(id);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult UserProfile(tbl_users Objuser, String cSharp , String Java,String Python)
+        {
+            if(cSharp == "true")
+            {
+                Objuser.InterestedInCSharp = true;
+            }
+            else
+            {
+                Objuser.InterestedInCSharp = false;
+            }
+            if (Java == "true")
+            {
+                Objuser.InterestedInJava = true;
+            }
+            else
+            {
+                Objuser.InterestedInJava = false;
+
+            }
+            if (Python == "true")
+            {
+                Objuser.InterestedInPython = true;
+            }
+            else
+            {
+                Objuser.InterestedInPython = false;
+
+            }
+
+            Entities db = new Entities();
+            db.Entry(Objuser).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("UsersList");
+        }
+        
+
+        [HttpGet]
+        
+        public ActionResult Edit(int id)
+        {
+            Entities db = new Entities();
+            var user = db.tbl_users.Find(id);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(tbl_users user)
+        {
+            Entities db = new Entities();
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("UserList");
+        }
+
+
+        public ActionResult Delete(int UserId)
+        {
+            Entities db = new Entities();
+            var user = db.tbl_users.Find(UserId);
+            db.tbl_users.Remove(user);
+            db.SaveChanges();
+            return View();
+        }
+
+    }
+}
---
+++ RegisterationSystem/Models/EmployeeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegisterationSystem.Models
+{
+    public class EmployeeViewModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public Nullable<bool> InterestedInCSharp { get; set; }
+        public Nullable<bool> InterestedInJava { get; set; }
+        public Nullable<bool> InterestedInPython { get; set; }
+
+       
+
+    }
+}
